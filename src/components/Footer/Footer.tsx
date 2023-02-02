df009f880e60ed9495b53c044f634fb5044c0ff2
Phillip-England <Phillip.mark.england@gmail.com>
2023-02-02 06:24:14 -0600

    working on layout for logged out clients

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className={'fixed bottom-0 p-4 w-screen flex'}>
+      <p className={'font-serif text-md'}>To have a positive influence</p>
+    </footer>
+  )
+}
--- src/layouts/LoggedOutLayout.tsx
+++ src/layouts/LoggedOutLayout.tsx
@@ -1,10 +1,12 @@
 
 import { Navbar } from "../components/Navbar/Navbar"
+import { Footer } from "../components/Footer/Footer"
 
 export const LoggedOutLayout = () => {
   return (
     <>
       <Navbar title={'Talent Management Portal'} />
+      <Footer />
     </>
   )
 }
